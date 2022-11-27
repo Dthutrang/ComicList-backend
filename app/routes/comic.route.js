01b4ecfa7afc85f221871aaee3147e216f232008
Dthutrang <daothutrang15082001@gmail.com>
2022-11-27 21:18:18 +0700

    Dinh nghia cac route cho tai nguyen Comic

---
+++ app/routes/comic.route.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const comics = require("../controllers/comic.controller");
+
+const router = express.Router();
+
+router.route("/").get(comics.findAll).post(comics.create);
+
+router.route("/favorite").get(comics.findAllFavorite);
+
+router
+  .route("/:id")
+  .get(comics.findOne)
+  .put(comics.update)
+  .delete(comics.delete);
+
+module.exports = router;
