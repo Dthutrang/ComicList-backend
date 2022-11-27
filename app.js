1b4ed6bbe0cf42409e272106eeaa7a92763750e6
Dthutrang <daothutrang15082001@gmail.com>
2022-11-27 21:25:02 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/comic", comicsRouter);
+app.use("/api/", comicsRouter);
 
 // handle 404 response
 app.use((req, res, next) => {
